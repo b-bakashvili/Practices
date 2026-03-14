ba59bee9a72ab90d268b69d7104fcd035bff5a35
b-bakashvili <BakashviliBeqa11@gmail.com>
2026-03-14 16:52:03 +0400

    Book, BookServices and Program.cs features

---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models;
+
+public class Book
+{
+    public int Id;
+    public string Title;
+    public string Author;
+    public string Genre;
+    public int PublishedYear;
+
+    public void printInfo()
+    {
+        Console.WriteLine("ID: " + Id);
+        Console.WriteLine("Title: " + Title);
+        Console.WriteLine("Author: " + Author);
+        Console.WriteLine("Genre: " + Genre);
+        Console.WriteLine("Published Year: " + PublishedYear);
+    }
+}
