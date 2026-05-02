e6138fe5c10b25f8fbb94984160e8cbd4e26c4dd
b-bakashvili <BakashviliBeqa11@gmail.com>
2026-05-02 15:27:18 +0400

    Add Practice12

---
+++ Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models;
+
+public class Book
+{
+    public int Id;
+    public string Title;
+    public string Author;
+    public string Genre;
+    public int PublishedYear;
+    public bool IsBorrowed;
+    public int? BorrowedByMemberId;
+    //public bool IsBorrowed { get; set; }
+    //public int? BorrowedByMemberId { get; set; }
+
+    public void printInfo()
+    {
+        Console.WriteLine("ID: " + Id);
+        Console.WriteLine("Title: " + Title);
+        Console.WriteLine("Author: " + Author);
+        Console.WriteLine("Genre: " + Genre);
+        Console.WriteLine("Published Year: " + PublishedYear);
+    }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models;
+
+public class Member
+{
+    public int Id;
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public string Email;
+    public string PersonalNumber;
+    public DateTime CreatedOn;
+
+    public void PrintInfo()
+    {
+        Console.WriteLine("ID: " + Id);
+        Console.WriteLine("Name: " + FirstName + LastName);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Email: " + Email);
+        Console.WriteLine("PersonalNumber: " + PersonalNumber);
+        Console.WriteLine("CreatedOn: " + CreatedOn.Date.ToShortDateString());
+        Console.WriteLine("----------------------");
+    }
+}
